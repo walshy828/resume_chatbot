fd5d816e48ed3054c0f765ad34ccfa07ac2b06f0
Dan Walsh <dan.walsh28@gmail.com>
2025-12-05 18:08:02 -0500

    config: add CORS_ALLOWED_ORIGINS to docker-compose and allow unsafe Werkzeug reloader

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,5 @@
 from app.api import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8181, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8181, debug=False, allow_unsafe_werkzeug=True)
+
